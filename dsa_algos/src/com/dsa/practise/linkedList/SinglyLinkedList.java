49485badf1001564970505cc61b2b6a1b37c603d
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-04-18 14:38:08 +0630

    bye

---
+++ dsa_algos/src/com/dsa/practise/linkedList/SinglyLinkedList.java
@@ -0,0 +1,29 @@
+package com.dsa.practise.linkedList;
+
+public class SinglyLinkedList {
+
+	private ListNode head;
+
+	private static class ListNode {
+		private int data;
+		private ListNode next;
+
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		SinglyLinkedList sll = new SinglyLinkedList();
+		sll.head = new ListNode(10);
+		ListNode second = new ListNode(1);
+		ListNode third = new ListNode(8);
+		ListNode fourth = new ListNode(11);
+		
+		sll.head.next = second;
+		second.next = third;
+		third.next = fourth;
+	}
+
+}
