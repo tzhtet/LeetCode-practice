c2f070639e6d533fc1578ab18f84cbd2e5abeccf
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-04-19 12:53:47 +0630

    update morning

--- dsa_algos/src/com/dsa/practise/linkedList/SinglyLinkedList.java
+++ dsa_algos/src/com/dsa/practise/linkedList/SinglyLinkedList.java
@@ -13,17 +13,43 @@ public class SinglyLinkedList {
 			this.next = null;
 		}
 	}
+
+	public void display() {
+		ListNode current = head;
+		while (current != null) {
+			System.out.print(current.data + "---> ");
+			current = current.next;
+		}
+		System.out.print("null");
+		System.out.println();
+	}
 	
+	public int length() {
+		if(head == null) {
+			return 0;
+		}
+		int count = 0;
+		ListNode current = head;
+		while(current != null) {
+			count ++;
+			current = current.next;
+		}
+		return count;
+	}
+
 	public static void main(String[] args) {
 		SinglyLinkedList sll = new SinglyLinkedList();
 		sll.head = new ListNode(10);
 		ListNode second = new ListNode(1);
 		ListNode third = new ListNode(8);
 		ListNode fourth = new ListNode(11);
-		
+
 		sll.head.next = second;
 		second.next = third;
 		third.next = fourth;
+		sll.display();
+		
+		System.out.println("Length is - " + sll.length());
 	}
 
 }
