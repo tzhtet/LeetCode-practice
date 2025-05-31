5065d03d47bf88de4a1ff88ce8b151fb14b351c0
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-05-31 23:16:03 +0630

    update

---
+++ dsa_algos/src/com/dsa/practise/linkedList/DetectALoop.java
@@ -0,0 +1,15 @@
+package com.dsa.practise.linkedList;
+
+public class DetectALoop {
+
+	
+	private class ListNode {
+		private int data;
+		private ListNode next;
+		
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+}
