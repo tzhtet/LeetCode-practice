02ec3956c94a3536d737c8f8fc89765c9fe373f9
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-06-02 13:30:26 +0630

    update

--- dsa_algos/src/com/dsa/practise/linkedList/DetectALoop.java
+++ dsa_algos/src/com/dsa/practise/linkedList/DetectALoop.java
@@ -33,9 +33,43 @@ public class DetectALoop {
 		System.out.println("null");
 	}
 	
-	public static void main(String[] args) {
+	public boolean containsLoop() {
+		ListNode fastPtr = head;
+		ListNode slowPtr = head;
 		
+		while(fastPtr != null && fastPtr.next != null) {
+			fastPtr = fastPtr.next.next;
+			slowPtr = slowPtr.next;
+			
+			if(slowPtr == fastPtr) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
+	public void createALoopInLinkedList() {
+		ListNode first = new ListNode(1);
+		ListNode second = new ListNode(2);
+		ListNode third = new ListNode(3);
+		ListNode fourth = new ListNode(4);
+		ListNode fifth = new ListNode(5);
+		ListNode sixth = new ListNode(6);
+		
+		head = first;
+		first.next = second;
+		second.next = third;
+		third.next = fourth;
+		fourth.next = fifth;
+		fifth.next = sixth;
+		sixth.next = third;
+	}
 	
+	public static void main(String[] args) {
+		DetectALoop det = new DetectALoop();
+		det.createALoopInLinkedList();
+		System.out.println(det.containsLoop());
+	}
+	
+		
 }
