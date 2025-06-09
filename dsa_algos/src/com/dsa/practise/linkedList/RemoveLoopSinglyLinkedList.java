5b1f69e26164a696fc9a2a7a0d85f713c024c1b1
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-06-09 15:05:20 +0630

    update

--- dsa_algos/src/com/dsa/practise/linkedList/RemoveLoopSinglyLinkedList.java
+++ dsa_algos/src/com/dsa/practise/linkedList/RemoveLoopSinglyLinkedList.java
@@ -8,7 +8,7 @@ public class RemoveLoopSinglyLinkedList {
 		private int data;
 		private ListNode next;
 
-		public ListNode() {
+		public ListNode(int data) {
 			this.data = data;
 			this.next = next;
 		}
@@ -23,5 +23,28 @@ public class RemoveLoopSinglyLinkedList {
 		}
 		System.out.println("null");
 		System.out.println();
+	}
+	
+	private void createALoopInLinkedList() {
+		ListNode first = new ListNode(1);
+		ListNode second = new ListNode(2);
+		ListNode third = new ListNode(3);
+		ListNode fourth = new ListNode(4);
+		ListNode fifth = new ListNode(5);
+		ListNode sixth = new ListNode(6);
+		
+		head = first;
+		first.next = second;
+		second.next = third;
+		third.next = fourth;
+		fourth.next = fifth;
+		fifth.next = sixth;
+		sixth.next = third;
+
+	}
+	public static void main(String[] args) {
+		RemoveLoopSinglyLinkedList rm = new RemoveLoopSinglyLinkedList();
+		rm.createALoopInLinkedList();
+		
 	}
 }
