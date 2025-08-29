e48b24085879c1dd6d157045d7060fc2cc6d607e
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-08-29 22:00:32 +0630

    just

--- dsa_algos/src/com/dsa/practise/list/SinglyLinkedList.java
+++ dsa_algos/src/com/dsa/practise/list/SinglyLinkedList.java
@@ -219,5 +219,25 @@ public class SinglyLinkedList {
 		temp.next = newNode;
 		return head;
 	}
+	
+	public void deleteNode(int key) {
+		ListNode current = head;
+		ListNode temp = null;
+		
+		if(current != null && current.data == key) {
+			head = current.next;
+			return;
+		}
+		
+		while(current != null && current.next.data != key) {
+			temp = current;
+			current = current.next;
+		}
+		
+		if(current == null) {
+			return;
+		}
+		temp.next = current.next;
+	}
 
 }
